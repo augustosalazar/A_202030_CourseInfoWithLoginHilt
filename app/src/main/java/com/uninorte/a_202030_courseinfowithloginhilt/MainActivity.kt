ac3e23ca8f1108e90273e4149a6bef3684a06196
augustosalazar <augustosalazar@uninorte.edu.co>
2020-09-15 17:56:44 -0500

    Initial commit

---
+++ app/src/main/java/com/uninorte/a_202030_courseinfowithloginhilt/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.uninorte.a_202030_courseinfowithloginhilt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+}
